2b7d5d13cf902a592ee69f5757b50c2ad61171d9
Mário Chaves <macndesign@gmail.com>
2016-01-10 20:07:09 -0200

    subscription detail com erros

---
+++ eventex/subscriptions/tests/test_view_detail.py
@@ -0,0 +1,32 @@
+from django.test import TestCase
+from eventex.subscriptions.models import Subscription
+
+
+class SubscriptionDetailGet(TestCase):
+    def setUp(self):
+        obj = Subscription.objects.create(
+            name='Henrique Bastos',
+            cpf='12345678901',
+            email='henrique@bastos.net',
+            phone='21-996186180'
+        )
+        self.resp = self.client.get('/inscricao/{}/'.format(obj.pk))
+
+    def test_get(self):
+        self.assertEqual(200, self.resp.status_code)
+
+    def test_template(self):
+        self.assertTemplateUsed(self.resp,
+                                'subscriptions/subscription_detail.html')
+
+    def test_context(self):
+        subscription = self.resp.context['subscription']
+        self.assertIsInstance(subscription, Subscription)
+
+    def test_html(self):
+        contents = ('Henrique Bastos', '12345678901',
+                    'henrique@bastos.net', '21-996186180')
+
+        with self.subTest():
+            for expected in contents:
+                self.assertContains(self.resp, expected)
--- eventex/subscriptions/tests/test_view_subscribe.py
+++ eventex/subscriptions/tests/test_view_subscribe.py
@@ -40,13 +40,13 @@ class SubscribeGet(TestCase):
 
 class SubscribePostValid(TestCase):
     def setUp(self):
-        data = dict(name='Mário Chaves', cpf='12345678901',
-                    email='macndesign@gmail.com', phone='11-95089-6310')
+        data = dict(name='Henrique Bastos', cpf='12345678901',
+                    email='henrique@bastos.net', phone='21-996186180')
         self.resp = self.client.post('/inscricao/', data)
 
     def test_post(self):
-        """Valid POST should redirect to /inscricao/"""
-        self.assertEqual(302, self.resp.status_code)
+        """Valid POST should redirect to /inscricao/1/"""
+        self.assertRedirects(self.resp, '/inscricao/1/')
 
     def test_send_subscribe_email(self):
         self.assertEqual(1, len(mail.outbox))
@@ -76,12 +76,3 @@ class SubscribePostInvalid(TestCase):
 
     def test_dont_save_subscription(self):
         self.assertFalse(Subscription.objects.exists())
-
-
-class SubscribeSuccessMessage(TestCase):
-    def test_message(self):
-        data = dict(name='Mário Chaves', cpf='12345678901',
-                    email='macndesign@gmail.com', phone='11-95089-6310')
-
-        response = self.client.post('/inscricao/', data, follow=True)
-        self.assertContains(response, 'Inscrição realizada com sucesso!')
