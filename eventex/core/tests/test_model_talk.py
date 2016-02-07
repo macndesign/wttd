f17ac7e713ac4b78a8c1fd5c5a27466284530696
Mário Chaves <macndesign@gmail.com>
2016-02-07 00:34:25 -0200

    Course como abstract bas class

--- eventex/core/tests/test_model_talk.py
+++ eventex/core/tests/test_model_talk.py
@@ -1,12 +1,12 @@
 from django.test import TestCase
 from eventex.core.managers import PeriodManager
-from eventex.core.models import Talk
+from eventex.core.models import Talk, Course
 
 
 class TalkModelTest(TestCase):
     def setUp(self):
         self.talk = Talk.objects.create(
-            title='Título da Palestra'
+                title='Título da Palestra'
         )
 
     def test_create(self):
@@ -15,9 +15,9 @@ class TalkModelTest(TestCase):
     def test_has_speakers(self):
         """Talk has many Speakers and vice-versa"""
         self.talk.speakers.create(
-            name='Henrique Bastos',
-            slug='henrique-bastos',
-            website='http://henriquebastos.net'
+                name='Henrique Bastos',
+                slug='henrique-bastos',
+                website='http://henriquebastos.net'
         )
         self.assertEqual(1, self.talk.speakers.count())
 
@@ -58,3 +58,32 @@ class PeriodManagerTest(TestCase):
         qs = Talk.objects.at_afternoon()
         expected = ['Afternoon Talk']
         self.assertQuerysetEqual(qs, expected, lambda o: o.title)
+
+
+class CourseModelTest(TestCase):
+    def setUp(self):
+        self.course = Course.objects.create(
+            title='Título do Curso',
+            start='09:00',
+            description='Descrição do curso.',
+            slots=20
+        )
+
+    def test_create(self):
+        self.assertTrue(Course.objects.exists())
+
+    def test_speakers(self):
+        """Course has many speakers and vice-versa"""
+        self.course.speakers.create(
+            name='Henrique Bastos',
+            slug='henrique-bastos',
+            website='http://henriquebastos.net'
+        )
+
+        self.assertEqual(1, self.course.speakers.count())
+
+    def test_str(self):
+        self.assertEqual('Título do Curso', str(self.course))
+
+    def test_manager(self):
+        self.assertIsInstance(Course.objects, PeriodManager)
